7e1ef42be8209be50264033e0058885e54fa771a
Destany <destany@zipcoders-mbp.mynetworksettings.com>
2023-07-17 22:03:00 -0400

    Completed

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
-//import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.HashMap;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -13,39 +10,55 @@ import java.util.Map;
  */
 public class PhoneBook {
 
-    private final Map<String, List<String>> phonebook;
-
-    public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
-    }
+    private  Map<String, List<String>> contacts;
 
     public PhoneBook() {
-        this(null);
+        this.contacts = new HashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, new ArrayList<>());
+        }
+        contacts.get(name).add(phoneNumber);
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, new ArrayList<>());
+        }
+        List<String> phoneNumberList = contacts.get(name);
+        phoneNumberList.addAll(Arrays.asList(phoneNumbers));
     }
 
     public void remove(String name) {
+        if(!contacts.containsKey(name)){
+            contacts.put(name, new ArrayList<>());
+        }
+        contacts.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return contacts.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return contacts.getOrDefault(name, new ArrayList<>());
     }
 
     public String reverseLookup(String phoneNumber)  {
+        for(Map.Entry<String, List<String>> entry : contacts.entrySet()){
+            String name = entry.getKey();
+            List<String> phoneNumbers = entry.getValue();
+            if(phoneNumbers.contains(phoneNumbers)){
+                return name;
+            }
+        }
         return null;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        return new ArrayList<>(contacts.keySet());
     }
 
     public Map<String, List<String>> getMap() {
